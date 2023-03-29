f06186c42104a4a0ef4d8865b5da356556924d5f
xkito24 <kuminsenpai1@gmail.com>
2023-03-29 22:49:49 +0700

    update tuan 3

--- BigSchool/Controllers/HomeController.cs
+++ BigSchool/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using BigSchool.ViewModels;
 
 namespace BigSchool.Controllers
 {
@@ -23,7 +24,13 @@ namespace BigSchool.Controllers
                 .Include(c => c.Lecturer)
                 .Include(c => c.Category)
                 .Where(c => c.DateTime > DateTime.Now);
-            return View(upcommingCourses);
+
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
     }
 }
